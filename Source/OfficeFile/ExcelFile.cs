bc43e9456b65a801bde99f8ff781cbb77e1acbd0
FL <felip3.fl@gmail.com>
2024-05-03 08:42:27 -0300

    New project - Read excel file

---
+++ Source/OfficeFile/ExcelFile.cs
@@ -0,0 +1,41 @@
+﻿using ExcelDataReader;
+
+namespace OfficeFile
+{
+    public class ExcelFile
+    {
+        public List<List<object>> Open(string filePath)
+        {
+            var excelData = new List<List<object>>();
+
+            using (var stream = System.IO.File.Open(filePath, FileMode.Open, FileAccess.Read))
+            {
+
+                using (var reader = ExcelReaderFactory.CreateReader(stream, new ExcelReaderConfiguration()
+                {
+                    //FallbackEncoding = Encoding.GetEncoding(1252)
+                }))
+                {
+
+                    List<string> excel = new List<string>();
+                    // 1. Use the reader methods
+                    do
+                    {
+                        while (reader.Read())
+                        {
+                            var rowData = new List<object>();
+                            for (int column = 0; column < reader.FieldCount; column++)
+                            {
+                                rowData.Add(reader.GetValue(column));
+                            }
+                            excelData.Add(rowData);
+
+
+                        }
+                    } while (reader.NextResult());
+                }
+            }
+            return excelData;
+        }
+    }
+}
