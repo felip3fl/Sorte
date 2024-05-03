c58bb33805a05a798e95894e7cc6c2dc37f0d204
FL <felip3.fl@gmail.com>
2024-05-03 08:42:54 -0300

    New controller Lotofacil

---
+++ Source/Oiretros/Controllers/LotofacilController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Oiretros.Controllers;
+using Oiretros;
+
+namespace API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class LotofacilController : Controller
+    {
+
+        private readonly ILogger<WeatherForecastController> _logger;
+
+        public LotofacilController(ILogger<WeatherForecastController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet(Name = "GetGames")]
+        public IEnumerable<WeatherForecast> Get()
+        {
+            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+                TemperatureC = Random.Shared.Next(-20, 55),
+            })
+            .ToArray();
+        }
+    }
+}
