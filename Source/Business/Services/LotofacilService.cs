5615417f9a3a7a2d483837099132293cb49c1e90
FL <felip3.fl@gmail.com>
2024-05-07 21:07:10 -0300

    New interfaces and services

--- Source/Business/Services/LotofacilService.cs
+++ Source/Business/Services/LotofacilService.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Business.Interfaces.Services;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,8 @@ using System.Threading.Tasks;
 
 namespace Business.Services
 {
-    public class LotofacilService
+    public class LotofacilService : ILotofacilService
     {
+
     }
 }
