5f85abf2053d208e7f5ff95f5899b9577d773074
FL <felip3.fl@gmail.com>
2024-05-17 20:29:13 -0300

    Improve JogoLotofacil, using List

--- Source/Business/Models/JogoLotofacil.cs
+++ Source/Business/Models/JogoLotofacil.cs
@@ -8,31 +8,13 @@ namespace Business.Models
 {
     public class JogoLotofacil
     {
+        public JogoLotofacil() 
+        {
+            Numeros = new List<byte>();
+        }
+
         public int Concurso { get; set; }
-        public bool NumberOne { get; set; }
-        public bool NumberTwo { get; set; }
-        public bool NumberThree { get; set; }
-        public bool NumberFour { get; set; }
-        public bool NumberFive { get; set; }
-        public bool NumberSix { get; set; }
-        public bool NumberSeven { get; set; }
-        public bool NumberEight { get; set; }
-        public bool NumberNine { get; set; }
-        public bool NumberTen { get; set; }
-        public bool NumberEleven { get; set; }
-        public bool NumberTwelve { get; set; }
-        public bool NumberThirteen { get; set; }
-        public bool NumberFourteen { get; set; }
-        public bool NumberFifteen { get; set; }
-        public bool NumberSixteen { get; set; }
-        public bool NumberSeventeen { get; set; }
-        public bool NumberEighteen { get; set; }
-        public bool NumberNineteen { get; set; }
-        public bool NumberTwenty { get; set; }
-        public bool NumberTwentyOne { get; set; }
-        public bool NumberTwentyTwo { get; set; }
-        public bool NumberTwentyThree { get; set; }
-        public bool NumberTwentyFour { get; set; }
-        public bool NumberTwentyFive { get; set; }
+        public List<byte> Numeros { get; set; }
+        
     }
 }
--- Source/Oiretros/Controllers/LotofacilController.cs
+++ Source/Oiretros/Controllers/LotofacilController.cs
@@ -24,108 +24,29 @@ namespace API.Controllers
             var result = _lotofacilService.Open("");
             var jogoLotofacil = ConvertListTojogoLotofacil(result);
 
-
-
             return Ok(jogoLotofacil);
         }
 
-        private JogoLotofacil ConvertListTojogoLotofacil(List<List<object>> result)
+        private Jogos ConvertListTojogoLotofacil(List<List<object>> result)
         {
-            StringBuilder result2 = new StringBuilder();
-            JogoLotofacil jogoLotofacil = new JogoLotofacil();
+            Jogos jogos = new Jogos();
+
+            result.Remove(result[0]);
 
             foreach (var item in result)
             {
-                for (var i = 2; i < 16; i++)
+                JogoLotofacil jogoLotofacil = new JogoLotofacil();
+
+                for (var i = 2; i < 17; i++)
                 {
-                    switch (item[i])
-                    {
-                        case 1:
-                            jogoLotofacil.NumberOne = true;
-                            break;
-
-                        case 2:
-                            jogoLotofacil.NumberTwo = true;
-                            break;
-
-                        case 3:
-                            jogoLotofacil.NumberThree = true;
-                            break;
-                        case 4:
-                            jogoLotofacil.NumberFour = true;
-                            break;
-                        case 5:
-                            jogoLotofacil.NumberFive = true;
-                            break;
-                        case 6:
-                            jogoLotofacil.NumberSix = true;
-                            break;
-                        case 7:
-                            jogoLotofacil.NumberSeven = true;
-                            break;
-                        case 8:
-                            jogoLotofacil.NumberEight = true;
-                            break;
-                        case 9:
-                            jogoLotofacil.NumberNine = true;
-                            break;
-                        case 10:
-                            jogoLotofacil.NumberTen = true;
-                            break;
-                        case 11:
-                            jogoLotofacil.NumberEleven = true;
-                            break;
-                        case 12:
-                            jogoLotofacil.NumberTwelve = true;
-                            break;
-                        case 13:
-                            jogoLotofacil.NumberThirteen = true;
-                            break;
-                        case 14:
-                            jogoLotofacil.NumberFourteen = true;
-                            break;
-                        case 15:
-                            jogoLotofacil.NumberFifteen = true;
-                            break;
-                        case 16:
-                            jogoLotofacil.NumberSixteen = true;
-                            break;
-                        case 17:
-                            jogoLotofacil.NumberSeventeen = true;
-                            break;
-                        case 18:
-                            jogoLotofacil.NumberEighteen = true;
-                            break;
-                        case 19:
-                            jogoLotofacil.NumberNineteen = true;
-                            break;
-                        case 20:
-                            jogoLotofacil.NumberTwenty = true;
-                            break;
-                        case 21:
-                            jogoLotofacil.NumberTwentyOne = true;
-                            break;
-                        case 22:
-                            jogoLotofacil.NumberTwentyTwo = true;
-                            break;
-                        case 23:
-                            jogoLotofacil.NumberTwentyThree = true;
-                            break;
-                        case 24:
-                            jogoLotofacil.NumberTwentyFour = true;
-                            break;
-                        case 25:
-                            jogoLotofacil.NumberTwentyFive = true;
-                            break;
-
-                    }
+                    var value = Convert.ToByte(item[i]);
+                    jogoLotofacil.Numeros.Add(value);
 
                 }
-                result2.Append("\n");
-
+                jogos.JogosLotofacil.Add(jogoLotofacil);
             }
 
-            return jogoLotofacil;
+            return jogos;
         }
     }
 }
